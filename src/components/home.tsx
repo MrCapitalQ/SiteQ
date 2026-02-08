dcf24c20de365bb34fac240a3be283e9487b9321
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2026-02-08 14:54:53 -0500

    Make home page under construction.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom";
 import { Page } from "./ui/page";
 
 export function Home() {
   return (
-    <Page>
+    <Page className="flex flex-col">
       <title>Home</title>
       <h1>Home Page</h1>
 
-      <ul className="list-disc">
-        <li><Link to="/jellyfin/welcome">Jellyfin</Link></li>
-      </ul>
+      <div className="flex flex-col min-[24rem]:flex-row self-center justify-center m-8 text-2xl">
+        <div className="text-center">
+          🚧
+        </div>
+        <div className="text-center">
+          Under construction
+        </div>
+        <div className="text-center">
+          🚧
+        </div>
+      </div>
+
     </Page>
   );
 }
